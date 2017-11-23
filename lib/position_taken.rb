5dbc1c945358f42315cb1e68d8f7906f59c4ef3c
James Ferguson <ncjamesferguson@gmail.com>
2017-11-23 01:06:27 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,8 @@
 # code your #position_taken? method here!
 def position_taken(board, index)
-
+ taken = false
+ if (board[index] == "" || board[index] == " " || board[index] == nil)
+   taken = false
+ end
+ 
 end
